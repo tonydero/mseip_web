d1f736479b6315eba3babf3260006b2fb60c5803
tonydero <tonyderocchis@gmail.com>
2019-06-04 13:17:02 -0600

    update

--- mseip_webpage.py
+++ mseip_webpage.py
@@ -1,9 +1,13 @@
 # Created on Wed 2019.05.22 17:31 UTC
 # by Tony DeRocchis
 
-from flask import Flask, flash, redirect, render_template, request, session, abort
+from flask import Flask, flash, redirect, render_template, request, session, abort, url_for
 
 app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return redirect(url_for('home'))
 
 @app.route("/home/")
 def home():
